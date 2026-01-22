178342a0cfb9a621083d1f5f8deea5dd27cb77ea
imperionite <lr.aimperial@mmdc.mcl.edu.ph>
2026-01-22 16:13:38 +0800

    :tada: (scaffolding) initial setup

--- routes/contact.js
+++ routes/contact.js
@@ -1,43 +1,8 @@
 const express = require("express");
-const passport = require("passport");
-const {
-  createContactMessage,
-  getAllContactMessages,
-  markMessageAsRead,
-} = require("../controllers/contact.controller");
-
-const router = express.Router();
 
 /**
  * TODO:
- * POST   /api/contact
- * - Public route
- * - Calls createContactMessage
+ * Connect controller functions to routes
  */
-router.post("/", createContactMessage);
-
-/**
- * TODO:
- * GET /api/contact
- * - Admin only
- * - JWT protected
- */
-router.get(
-  "/",
-  passport.authenticate("jwt", { session: false }),
-  getAllContactMessages,
-);
-
-/**
- * TODO:
- * PATCH /api/contact/:id/read
- * - Admin only
- * - JWT protected
- */
-router.patch(
-  "/:id/read",
-  passport.authenticate("jwt", { session: false }),
-  markMessageAsRead,
-);
 
 module.exports = router;
--- routes/subscription.js
+++ routes/subscription.js
@@ -1,44 +1,7 @@
 const express = require("express");
-const passport = require("passport");
-const {
-  subscribe,
-  unsubscribe,
-  getAllSubscriptions,
-  sendNewsletter,
-} = require("../controllers/subscription.controller");
-
-const router = express.Router();
 
 /**
- * POST /api/subscriptions
- * - Public
+ * TODO:
+ * Connect controller functions to routes
  */
-router.post("/", subscribe);
-
-/**
- * DELETE /api/subscriptions
- * - Public
- */
-router.delete("/", unsubscribe);
-
-/**
- * GET /api/subscriptions
- * - Admin only
- */
-router.get(
-  "/",
-  passport.authenticate("jwt", { session: false }),
-  getAllSubscriptions,
-);
-
-/**
- * POST /api/subscriptions/send
- * - Admin only
- */
-router.post(
-  "/send",
-  passport.authenticate("jwt", { session: false }),
-  sendNewsletter,
-);
-
 module.exports = router;
